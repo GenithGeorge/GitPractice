6008d8236534e44ba23f16a981e61c7fe73fcd68
91918 <91918@DESKTOP-688O59M>
2022-09-30 15:13:23 +0530

    First Commit

---
+++ Object Repository/Dummy.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Dummy</name>
+   <tag></tag>
+   <elementGuidId>7a7d747e-e7b7-449f-8590-429c731b8a66</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Test Suites/Dummy.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Dummy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>da600d34-80be-409d-afae-976844c38166</testSuiteGuid>
+</TestSuiteEntity>
